c4d697426266072f621e5a90e3df4dda85578cdc
Eric P <eric.1334@icloud.com>
2021-10-21 21:22:49 -0700

    Working node communication

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
-mod node;
-
-#[macro_use]
-extern crate lazy_static;
-
 use std::net::{TcpListener, TcpStream};
 use std::thread::spawn;
 use url::Url;
-use tungstenite::{accept, connect, Message, WebSocket};
-use tungstenite::stream::MaybeTlsStream;
 use std::collections::HashMap;
 use serde_json::{Value, json};
 use serde::{Deserialize, Serialize};
@@ -15,13 +8,8 @@ use crate::LC::MessageType;
 
 use std::sync::{Mutex, Arc};
 
-
-lazy_static! {
-    static ref SOCKETS: Mutex<HashMap<String, WebSocket<TcpStream>>> = {
-        Mutex::new(HashMap::new())
-    };
-}
-
+extern crate ws;
+use ws::{connect, listen, CloseCode, Handler, Message, Result, Sender, Handshake};
 
 mod LC {
     use serde::{Deserialize, Serialize};
@@ -34,112 +22,154 @@ mod LC {
     }
 
     #[derive(Serialize, Deserialize, Debug)]
-    pub struct Message<'a> {
+    pub struct Message {
         pub typ: MessageType,
-        pub action: Option<&'a str>,
-        pub data: Option<&'a str>,
+        pub action: Option<String>,
+        pub data: Option<String>,
     }
 }
 
+type Connections = Arc<Mutex<HashMap<String, Sender>>>;
 
-fn run_server(node_ips: Arc<Mutex<Vec<String>>>) {
+struct Node {
+    connections: Connections
+}
 
-    let localhost = "0.0.0.0:9001";
-    let server = TcpListener::bind(localhost).unwrap();
+impl Clone for Node {
+    fn clone(&self) -> Self {
+        Node {
+            connections: Arc::clone(&self.connections)
+        }
+    }
+}
 
+impl Default for Node {
+    fn default() -> Self {
+        Node {
+            connections: Arc::new(Mutex::new(HashMap::new()))
+        }
+    }
+}
+
+impl Node {
+    fn add_new_connection(&mut self, socket: &Sender, ip_addr: String) {
+        let mut map = self.connections.lock().unwrap();
+        map.insert(ip_addr, socket.clone());
+    }
+
+    fn get_connections(&mut self) -> Vec<String> {
+        let mut map = self.connections.lock().unwrap();
+        map.keys().cloned().collect()
+    }
+}
+
+struct Server {
+    socket: Sender,
+    node: Node
+}
+
+impl Handler for Server {
+    fn on_open(&mut self, shake: Handshake) -> Result<()> {
+
+        let ip_addr = shake.remote_addr()?.unwrap();
+        self.node.add_new_connection(&self.socket, ip_addr);
+
+        self.request_nodes();
+
+
+        Ok(())
+    }
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        self.handle_message(msg);
+
+        Ok(())
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        println!("Client connection closing: {}", reason)
+    }
+}
+
+impl Server {
+    fn request_nodes(&self) {
+        let request = json!(LC::Message {
+                        typ: LC::MessageType::Request,
+                        action: Some("get_nodes".to_string()),
+                        data: None,
+                    });
+        self.socket.send(request.to_string());
+    }
+
+    fn handle_message(&mut self, msg: Message) {
+        match msg {
+            Message::Text(s) => {
+                println!("{:?}", s);
+                let parsed: LC::Message = serde_json::from_str(&s).unwrap();
+
+                match parsed.typ {
+                    MessageType::Request => {
+                        match parsed.action {
+                            Some(action) => {
+                                if(action == "get_nodes") {
+                                    let connections = self.node.get_connections();
+                                    let resp_data: String = connections.into_iter().collect();
+
+                                    let response = json!(LC::Message {
+                                        typ: LC::MessageType::Response,
+                                        action: None,
+                                        data: Some(resp_data)
+                                    });
+
+                                    self.socket.send(response.to_string());
+                                }
+                            },
+                            _ => {}
+                        }
+                    },
+                    _ => {}
+                }
+            },
+            _ => {}
+        }
+    }
+}
+
+fn run_server(node: Node) {
+
+    let localhost = "0.0.0.0:9002";
+
+    spawn(move || listen(localhost, 
+        |socket| {
+            Server {
+                socket: socket,
+                node: node.clone()
+            }
+        }).unwrap()
+    );
     println!("Running server!");
+}
+
+fn run_client(node: Node) {
+    let GOD: String = "ws://10.8.57.232:9001".to_string();
 
     spawn(move || {
-        for stream in server.incoming() {
-
-            let thread_ips = Arc::clone(&node_ips);
-            spawn(move || {
-
-                let mut websocket = accept(stream.unwrap()).unwrap();
-                println!("Something connected to server");
-                let ip_addr = websocket.get_ref().peer_addr().unwrap().to_string();
-                thread_ips.lock().unwrap().push(String::from(&ip_addr));
-
-                loop {
-                    println!("test");
-                    
-                    let msg = websocket.read_message().unwrap().into_text().unwrap(); 
-                    println!("{:?}", msg);
-
-                    let s: LC::Message = serde_json::from_str(&msg).unwrap();
-
-                    match s.typ {
-                        MessageType::Request => {
-                            match s.action {
-                                Some("get_nodes") => {
-                                    println!("YO!");
-
-
-                                    // let a: Vec<&String> = sockets.get_ .keys().into_iter().collect();
-
-
-                                    let resp = json!(LC::Message {
-                                    typ: LC::MessageType::Response,
-                                    action: None,
-                                    data: Some(json!(ip_addr).to_string().as_str())
-                                    });
-                                    let send =websocket.write_message(Message::text(resp.to_string()));
-                                }
-                                _ => {}
-                            }
-                        }
-                        MessageType::Response => {}
-                    }
-                }
-
-            });
-        }
+        connect(GOD, |socket| {
+            Server {
+                socket: socket,
+                node: node.clone()
+            }
+        }).unwrap()
     });
 }
 
-fn run_client(node_ips: Arc<Mutex<Vec<String>>>) {
-    let GOD: String = "ws://10.8.57.232:9001".to_string();
-    let (mut socket, response) = connect(Url::parse(&GOD).unwrap()).unwrap();
+fn main() {
+    let node: Node = Default::default();
 
-    println!("client connected to server");
+    run_server(node.clone());
 
-    // spawn(|| {
-    //     let msg = socket.read_message().unwrap();
-    //     println!("{:?}", msg);
-    // });
-    println!("{:?}", response);
-
-    let request = json!(LC::Message {
-                    typ: LC::MessageType::Request,
-                    action: Some("get_nodes"),
-                    data: None,
-                });
-    socket.write_message(Message::text(request.to_string()));
-
-    // has to be a FAST reader
-    //let msg = socket.read_message().unwrap();
-    //println!("{:?}", msg);
-
+    run_client(node.clone());
 
     loop {
-        let response = socket.read_message();
-    
-         if response.is_ok() {
-             let msg = response.unwrap();
-             if msg.is_text() {
-                 let text = msg.into_text().unwrap();
-    
-    
-                 println!("{:?}", text);
-             }
-         }
-     }
-}
-
-fn main() {
-    let node_ips: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
-
-    run_server(Arc::clone(&node_ips));
-
-    run_client(Arc::clone(&node_ips));
+    }
 }
