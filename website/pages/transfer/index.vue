3523b346d820ec5bcb27ddce112254a3dad2ee6f
0xzachvai <0xzachvai@gmail.com>
2022-01-12 03:22:15 -0800

    move styling

--- website/pages/transfer/index.vue
+++ website/pages/transfer/index.vue
@@ -14,21 +14,7 @@
     </div>
 </template>
 
-<style>
-    /* TODO: not scoped but not here either plz*/
-
-    .container {
-        margin: 64px auto;
-
-        background: white;
-        max-width: 480px;
-        /* width: fit-content; */
-
-        border-radius: 16px;
-        padding: 32px;
-        box-shadow: 0 0.5px 4px 0.25px #33333388;
-    }
-
+<style scoped>
     .input-row {
         margin: 16px 0;
     }
@@ -38,25 +24,5 @@
 
         border-radius: 8px;
         border: 2px solid black;
-    }
-
-    button {
-        cursor: pointer;
-        transition: all 0.3s;
-        width: 100%;
-
-        background: #ffd700;
-        color: black;
-
-        border: 2px solid black;
-        border-radius: 8px;
-
-        padding: 8px 12px;
-        font-size: 20px;
-        font-weight: 600;
-    }
-
-    button:hover {
-        background: #ecc900;
     }
 </style>
